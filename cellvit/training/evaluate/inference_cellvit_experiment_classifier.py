19f3959ac80d8fc027f26ec639d88541c68b5d61
Celestenono <noemie.moreau96@gmail.com>
2025-04-01 16:32:52 +0200

    error empty results

--- cellvit/training/evaluate/inference_cellvit_experiment_classifier.py
+++ cellvit/training/evaluate/inference_cellvit_experiment_classifier.py
@@ -638,7 +638,6 @@ class CellViTClassifierInferenceExperiment(ABC):
                 enumerate(inference_embedding_dataloader),
                 total=len(inference_embedding_dataloader),
             )
-            print(len(inference_embedding_dataloader))
             for _, batch in inference_loop:
                 cell_tokens = batch[0].to(
                     self.device
@@ -650,8 +649,6 @@ class CellViTClassifierInferenceExperiment(ABC):
                 class_predictions, probs = self._get_classifier_batch_result(
                     cell_tokens, threshold
                 )
-                print(class_predictions)
-                print(cell_types)
                 classifier_output["predictions"].append(class_predictions)
                 classifier_output["probabilities"].append(probs)
                 classifier_output["gt"].append(cell_types)
